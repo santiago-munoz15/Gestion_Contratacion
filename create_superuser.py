3be24d2f72194c77889600b087ae2d08f01300f2
200~juandiegoosorio00001 <juan.osorio79@correo.tdea.edu.co>
2026-05-21 20:58:25 -0500

    conexion postgres render

--- create_superuser.py
+++ create_superuser.py
@@ -1,32 +1,11 @@
-import os
-import django
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gestion_contratacion.settings')
-django.setup()
-
 from accounts.models import User
 
-username = 'juan3'
-
-if not User.objects.filter(username=username).exists():
-
-    user = User.objects.create(
-        username='juan3',
-        email='juan3@gmail.com',
-        role='candidato',
-        is_active=True
+if not User.objects.filter(username='juan').exists():
+    User.objects.create_user(
+        username='juan',
+        password='123456',
+        role='candidato'
     )
-
-    user.set_password('123456')
-    user.save()
-
-    print('Usuario creado correctamente')
-
+    print("Usuario creado")
 else:
-    user = User.objects.get(username=username)
-
-    user.set_password('123456')
-    user.is_active = True
-    user.save()
-
-    print('Usuario actualizado correctamente')
+    print("Ya existe")
