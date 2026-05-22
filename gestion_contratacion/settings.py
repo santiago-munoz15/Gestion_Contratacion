89d6ab0ddca084196f947b169f6fbd652ee8c40a
200~juandiegoosorio00001 <juan.osorio79@correo.tdea.edu.co>
2026-05-22 16:10:13 -0500

    fix postgres render

--- gestion_contratacion/settings.py
+++ gestion_contratacion/settings.py
@@ -107,12 +107,15 @@ WSGI_APPLICATION = 'gestion_contratacion.wsgi.application'
 import dj_database_url
 import os
 
+DATABASE_URL = os.getenv("DATABASE_URL")
+
 DATABASES = {
-    'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
+    "default": dj_database_url.parse(
+        DATABASE_URL,
+        conn_max_age=600,
+        ssl_require=True
     )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
